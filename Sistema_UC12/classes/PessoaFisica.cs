fbe431e173bf44c429c21fcd3209927e384fb11d
Alex Junior <113397859+AlexJunior-dev@users.noreply.github.com>
2023-05-02 23:39:44 -0300

    subindo aula

--- Sistema_UC12/classes/PessoaFisica.cs
+++ Sistema_UC12/classes/PessoaFisica.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Sistema_UC12.classes
 {
     public class PessoaFisica : Pessoa
@@ -12,5 +13,22 @@ namespace Sistema_UC12.classes
        public string? rg { get; set; } 
 
        public DateTime dataNascimento { get; set; }
+
+       public override void pagarImposto(float rendimento){
+
+       }
+
+       public bool validarDataNascimento(DateTime DataNasc){
+            DateTime dataAtual = DateTime.Today;
+        
+            double anos = (dataAtual - dataNascimento).TotalDays / 365;
+
+
+            if (anos >= 18){
+                return true;   
+            }else{
+                return false;
+            }
+        }
     }
 }
--- Sistema_UC12/classes/PessoaJuridica.cs
+++ Sistema_UC12/classes/PessoaJuridica.cs
@@ -10,6 +10,20 @@ namespace Sistema_UC12.classes
         public string? cnpj { get; set; }
 
         public string? razaoSocial { get; set; }
+
+        public override void pagarImposto(float rendimento){
+
+       }
+
+       public bool ValidarCNPJ(string cnpj)
+       {
+           if (cnpj.Length >= 14 && (cnpj.Substring(cnpj.Length - 4)) == "0001")
+           {
+            return true;
+           } else{
+           return false;
+           }
+       }   
     
     }
 }
